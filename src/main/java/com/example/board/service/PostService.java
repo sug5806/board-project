b4b63a74a71cff5c9dac4238c9ca9bac85b4ec5b
홍동수 <sug5806@naver.com>
2021-03-22 17:34:41 +0900

    Post Service 작성 및 create 기능 작성

--- src/main/java/com/example/board/service/PostService.java
+++ src/main/java/com/example/board/service/PostService.java
@@ -1,8 +1,24 @@
 package com.example.board.service;
 
+import com.example.board.dto.PostDTO;
+import com.example.board.entitiy.Post;
+import com.example.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
 
+    public Long createPost(PostDTO postDTO) {
+        Post post = Post.builder()
+                .title(postDTO.getTitle())
+                .contents(postDTO.getContents())
+                .build();
+
+        Post savePost = postRepository.save(post);
+
+        return savePost.getId();
+    }
 }
