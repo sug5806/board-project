dd5fdd78b33d3751c6896b2d42e331c91bf5cf3e
홍동수 <sug5806@naver.com>
2021-03-23 16:43:53 +0900

    기본 적인 UI 구성

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -4,9 +4,6 @@ import com.example.board.entitiy.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> findByEmail(String email);
 }
