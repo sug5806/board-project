451a697758575918cec8c26c85060501ff802365
홍동수 <sug5806@naver.com>
2021-03-24 15:42:24 +0900

    전반적인 ui 구조 설정

--- src/main/java/com/example/board/controller/MainController.java
+++ src/main/java/com/example/board/controller/MainController.java
@@ -1,12 +1,14 @@
 package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String Main() {
+    public String Main(Model model) {
+        model.addAttribute("type", "free");
         return "main";
     }
 }
