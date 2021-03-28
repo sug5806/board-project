17b973b0c91ab711776dd9980b2a6ad61343be4b
홍동수 <sug5806@naver.com>
2021-03-28 11:51:06 +0900

    작성일, 작성자, 수정일, 수정자 추가 및 로그인 버튼 추가

--- src/main/java/com/example/board/entity/Post.java
+++ src/main/java/com/example/board/entity/Post.java
@@ -1,5 +1,6 @@
 package com.example.board.entity;
 
+import com.example.board.entity.common.BaseEntity;
 import lombok.*;
 
 import javax.persistence.*;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post {
+public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
